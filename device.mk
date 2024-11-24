38ea12249be67388083ce5867560e40225ece780
Deivid21 <david.parra.ignacio@gmail.com>
2024-11-24 01:56:31 -0300

    guamp: Enable TARGET_USES_MOTCAMERA2
    Change-Id: If1c19dca513a8ce031d45c2b5b3d715b1d6ddbf9
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -36,6 +36,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Moto Camera 2
+TARGET_MOTCAMERA2 := whitney
+TARGET_USES_MOTCAMERA2 := true
+
+$(call inherit-product, vendor/motorola/MotCamera2/motcamera2.mk)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service

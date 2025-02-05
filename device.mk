3aaa12a9b2dc0e04705092e708156dc7b093cfee
4mede <4mede@tutamail.com>
2025-02-05 14:57:15 -0500

    guamp: remove conflict marker

--- device.mk
+++ device.mk
@@ -18,7 +18,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Boot animation
-<<<<<<< HEAD
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
 TARGET_BOOTANIMATION_HALF_RES := true

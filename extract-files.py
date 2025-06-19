7364687052f098e4e43115ee1d5ad8ec24b3e513
Debarpan102 <debarpanhalder8@gmail.com>
2025-06-19 17:30:00 +0530

    borneo: Remove Firmware Extraction

--- extract-files.py
+++ extract-files.py
@@ -5,7 +5,6 @@
 #
 
 from extract_utils.extract import extract_fns_user_type
-from extract_utils.extract_star import extract_star_firmware
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -42,10 +41,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcomparetf2_shim.so'),
 }  # fmt: skip
 
-extract_fns: extract_fns_user_type = {
-    r'(bootloader|radio)\.img': extract_star_firmware,
-}
-
 module = ExtractUtilsModule(
     'guamp',
     'motorola',
@@ -53,7 +48,6 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     extract_fns=extract_fns,
-    add_firmware_proprietary_file=True,
     add_generated_carriersettings=True,
 )
 
